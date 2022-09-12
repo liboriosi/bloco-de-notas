939f75897944aebf9d07c8b09e576533065f4c44
Gabriel Libório <79881759+liboriosi@users.noreply.github.com>
2022-09-12 01:50:03 -0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,14 +1,14 @@
 const staticBloquinho = "bloco-de-notas"
 const assets = [
-    "/",
-    "/index.html",
-    "/assets/styles/bootstrap.css",
-    "/assets/scripts/bootstrap.js",
-    "/assets/scripts/script.js",
-    "/assets/images/icon-192x192.png",
-    "/assets/images/icon-256x256.png",
-    "/assets/images/icon-384x384.png",
-    "/assets/images/icon-512x512.png",
+    "/bloco-de-notas",
+    "/bloco-de-notas/index.html",
+    "/bloco-de-notas/assets/styles/bootstrap.css",
+    "/bloco-de-notas/assets/scripts/bootstrap.js",
+    "/bloco-de-notas/assets/scripts/script.js",
+    "/bloco-de-notas/assets/images/icon-192x192.png",
+    "/bloco-de-notas/assets/images/icon-256x256.png",
+    "/bloco-de-notas/assets/images/icon-384x384.png",
+    "/bloco-de-notas/assets/images/icon-512x512.png",
 ]
 
 self.addEventListener("install", installEvent => {
